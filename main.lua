9d250e885ee745320fe447f91b7fa2478d2c76b9
municipaaall <municipaal@outlook.com>
2025-06-21 12:35:30 +0800

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,38 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()'
+
+local Window = Rayfield:CreateWindow({
+   Name = "Nocturne - Hub",
+   Icon = 0, 
+   LoadingTitle = "Games Await",
+   LoadingSubtitle = "by Municipal",
+   ShowText = "Nocturne", 
+   Theme = Default, 
+
+   ToggleUIKeybind = "K", 
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false,
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, 
+      FileName = "NocturneHub"
+   },
+
+   Discord = {
+      Enabled = true, 
+      Invite = "E3as8ezqRV",
+      RememberJoins = false
+   },
+
+   KeySystem = true ,
+   KeySettings = {
+      Title = "Nocturne Key System",
+      Subtitle = "Key System",
+      Note = "Key will be provided via dev",
+      FileName = "NocturneKey",
+      SaveKey = true, 
+      GrabKeyFromSite = false,
+      Key = {"Hello"}
+   }
+})
